b58719b147172d93ac7a60562835bfb8aefbe98a
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2017-11-14 13:50:29 +0900

    hello, world

---
+++ Objective-cTutorial/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-cTutorial
+//
+//  Created by tomatoaiu on 2017/11/14.
+//  Copyright © 2017年 tomatoaiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
